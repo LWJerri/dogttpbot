f3d4491a4727864cc55c7c35f63eaea782a8d5a5
LWJerri <loginmanx@gmail.com>
2023-09-06 14:24:03 +0300

    Move all composers code to `app.ts` due Vercel didn't detect `utils` folder

--- api/app.ts
+++ api/app.ts
@@ -1,7 +1,5 @@
 import "dotenv/config";
-import { Bot, webhookCallback } from "grammy";
-import { inlineQueryComposer } from "./utils/composers/inlineQuery";
-import { startComposer } from "./utils/composers/start";
+import { Bot, InlineKeyboard, InlineQueryResultBuilder, webhookCallback } from "grammy";
 import { devBootstrap } from "./utils/devBootstrap";
 
 const { BOT_TOKEN, NGROK_TOKEN, NODE_ENV } = process.env;
@@ -16,6 +14,40 @@ if (NODE_ENV === "development" && !NGROK_TOKEN) {
   await devBootstrap();
 }
 
-bot.use(startComposer).use(inlineQueryComposer);
+bot.command("start", async (ctx) => {
+  const keyboard = new InlineKeyboard().switchInlineCurrent("Search in this chat").row().switchInline("Share HTTP dog");
+
+  const messages = [
+    "I can search for HTTP dog inline.",
+    "This bot [open-source](github.com/LWJerri/dogttpbot) and uses [grammY](https://grammy.dev) 🌈",
+  ];
+
+  await ctx.reply(messages.join("\n\n"), {
+    reply_to_message_id: ctx.message.message_id,
+    parse_mode: "Markdown",
+    disable_web_page_preview: true,
+    reply_markup: keyboard,
+  });
+});
+
+bot.on("inline_query", async (ctx) => {
+  const { id, query } = ctx.inlineQuery;
+
+  if (!query) return;
+
+  const request = await fetch(`https://http.dog/${query}.jpg`);
+  const isCodeFound = request.headers.get("content-type") === "image/jpeg";
+
+  const buildPhotoUrl = `https://http.dog/${isCodeFound ? query : "404"}.jpg`;
+  const mdnDocsURL = `[More info about ${query} code.](https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/${query})`;
+
+  const inlineQueryResponse = InlineQueryResultBuilder.photo(id, buildPhotoUrl, {
+    thumbnail_url: buildPhotoUrl,
+    caption: isCodeFound ? mdnDocsURL : "Requested HTTP code not found 😭",
+    parse_mode: "Markdown",
+  });
+
+  await ctx.answerInlineQuery([inlineQueryResponse], { cache_time: 60 * 60 * 24 });
+});
 
 export default webhookCallback(bot, "https");
